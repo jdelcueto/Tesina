533e4963d534bbc8e0506230d2cd145affd9f409
Joel Del Cueto <jdelcueto@gmail.com>
2017-12-01 11:11:19 -0400

    reworking

---
+++ manuscript/ch3/.dir-locals.el
@@ -0,0 +1,14 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode
+  (org-hide-emphasis-markers . t)
+  (truncate-lines . t)
+  (word-wrap . t)
+  (mode . auto-fill))
+ (org-mode
+  (mode . org)))
+
+
+
+
