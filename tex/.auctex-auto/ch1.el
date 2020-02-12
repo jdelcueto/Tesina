65cb06a11ea36c9544d1fbf91b29b12b8e7aa0a8
Joel Del Cueto <jdelcueto@gmail.com>
2020-02-11 21:14:12 -0400

    working on ch1

--- tex/.auctex-auto/ch1.el
+++ tex/.auctex-auto/ch1.el
@@ -5,7 +5,6 @@
                      '(("subfiles" "../main.tex")))
    (TeX-run-style-hooks
     "latex2e"
-    "./ch1/magisterium"
     "./ch1/test_et_revelatio"
     "./ch1/quaestio_ipsius"
     "subfiles"
